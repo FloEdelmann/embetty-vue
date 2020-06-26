cff6e134e710678079d6140724f7534a13953e0a
Flo Edelmann <florian-edelmann@online.de>
2020-06-26 16:05:15 +0000

    Version 1.3.1

--- dist/embetty-vue.common.js
+++ dist/embetty-vue.common.js
@@ -213,7 +213,7 @@ var shared = createCommonjsModule(function (module) {
 (module.exports = function (key, value) {
   return sharedStore[key] || (sharedStore[key] = value !== undefined ? value : {});
 })('versions', []).push({
-  version: '3.6.4',
+  version: '3.6.5',
   mode:  'global',
   copyright: '© 2020 Denis Pushkarev (zloirock.ru)'
 });
@@ -1314,7 +1314,13 @@ if (!set$1 || !clear) {
     defer = functionBindContext(port.postMessage, port, 1);
   // Browsers with postMessage, skip WebWorkers
   // IE8 has postMessage, but it's sync & typeof its postMessage is 'object'
-  } else if (global_1.addEventListener && typeof postMessage == 'function' && !global_1.importScripts && !fails(post)) {
+  } else if (
+    global_1.addEventListener &&
+    typeof postMessage == 'function' &&
+    !global_1.importScripts &&
+    !fails(post) &&
+    location.protocol !== 'file:'
+  ) {
     defer = post;
     global_1.addEventListener('message', listener, false);
   // IE8-
@@ -1927,90 +1933,80 @@ var script = {
   }
 };
 
-function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier
-/* server only */
-, shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
-  if (typeof shadowMode !== 'boolean') {
-    createInjectorSSR = createInjector;
-    createInjector = shadowMode;
-    shadowMode = false;
-  } // Vue.extend constructor export interop.
-
-
-  var options = typeof script === 'function' ? script.options : script; // render functions
-
-  if (template && template.render) {
-    options.render = template.render;
-    options.staticRenderFns = template.staticRenderFns;
-    options._compiled = true; // functional template
-
-    if (isFunctionalTemplate) {
-      options.functional = true;
+function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier /* server only */, shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
+    if (typeof shadowMode !== 'boolean') {
+        createInjectorSSR = createInjector;
+        createInjector = shadowMode;
+        shadowMode = false;
     }
-  } // scopedId
-
-
-  if (scopeId) {
-    options._scopeId = scopeId;
-  }
-
-  var hook;
-
-  if (moduleIdentifier) {
-    // server build
-    hook = function hook(context) {
-      // 2.3 injection
-      context = context || // cached call
-      this.$vnode && this.$vnode.ssrContext || // stateful
-      this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext; // functional
-      // 2.2 with runInNewContext: true
-
-      if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
-        context = __VUE_SSR_CONTEXT__;
-      } // inject component styles
-
-
-      if (style) {
-        style.call(this, createInjectorSSR(context));
-      } // register component module identifier for async chunk inference
-
-
-      if (context && context._registeredComponents) {
-        context._registeredComponents.add(moduleIdentifier);
-      }
-    }; // used by ssr in case component is cached and beforeCreate
-    // never gets called
-
-
-    options._ssrRegister = hook;
-  } else if (style) {
-    hook = shadowMode ? function () {
-      style.call(this, createInjectorShadow(this.$root.$options.shadowRoot));
-    } : function (context) {
-      style.call(this, createInjector(context));
-    };
-  }
-
-  if (hook) {
-    if (options.functional) {
-      // register for functional component in vue file
-      var originalRender = options.render;
-
-      options.render = function renderWithStyleInjection(h, context) {
-        hook.call(context);
-        return originalRender(h, context);
-      };
-    } else {
-      // inject component registration as beforeCreate hook
-      var existing = options.beforeCreate;
-      options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+    // Vue.extend constructor export interop.
+    const options = typeof script === 'function' ? script.options : script;
+    // render functions
+    if (template && template.render) {
+        options.render = template.render;
+        options.staticRenderFns = template.staticRenderFns;
+        options._compiled = true;
+        // functional template
+        if (isFunctionalTemplate) {
+            options.functional = true;
+        }
     }
-  }
-
-  return script;
+    // scopedId
+    if (scopeId) {
+        options._scopeId = scopeId;
+    }
+    let hook;
+    if (moduleIdentifier) {
+        // server build
+        hook = function (context) {
+            // 2.3 injection
+            context =
+                context || // cached call
+                    (this.$vnode && this.$vnode.ssrContext) || // stateful
+                    (this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext); // functional
+            // 2.2 with runInNewContext: true
+            if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+                context = __VUE_SSR_CONTEXT__;
+            }
+            // inject component styles
+            if (style) {
+                style.call(this, createInjectorSSR(context));
+            }
+            // register component module identifier for async chunk inference
+            if (context && context._registeredComponents) {
+                context._registeredComponents.add(moduleIdentifier);
+            }
+        };
+        // used by ssr in case component is cached and beforeCreate
+        // never gets called
+        options._ssrRegister = hook;
+    }
+    else if (style) {
+        hook = shadowMode
+            ? function (context) {
+                style.call(this, createInjectorShadow(context, this.$root.$options.shadowRoot));
+            }
+            : function (context) {
+                style.call(this, createInjector(context));
+            };
+    }
+    if (hook) {
+        if (options.functional) {
+            // register for functional component in vue file
+            const originalRender = options.render;
+            options.render = function renderWithStyleInjection(h, context) {
+                hook.call(context);
+                return originalRender(h, context);
+            };
+        }
+        else {
+            // inject component registration as beforeCreate hook
+            const existing = options.beforeCreate;
+            options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+        }
+    }
+    return script;
 }
-
-var normalizeComponent_1 = normalizeComponent;
 
 /* script */
 const __vue_script__ = script;
@@ -2034,7 +2030,7 @@ const __vue_is_functional_template__ = undefined;
 
 /* style inject shadow dom */
 
-const __vue_component__ = normalizeComponent_1({}, __vue_inject_styles__, __vue_script__, __vue_scope_id__, __vue_is_functional_template__, __vue_module_identifier__, false, undefined, undefined, undefined);
+const __vue_component__ = /*#__PURE__*/normalizeComponent({}, __vue_inject_styles__, __vue_script__, __vue_scope_id__, __vue_is_functional_template__, __vue_module_identifier__, false, undefined, undefined, undefined);
 
 var LINK_IMAGE_SIZE = 125;
 var MIN_WINDOW_WIDTH = 600;
@@ -2276,60 +2272,58 @@ var script$1 = {
   }
 };
 
-var isOldIE = typeof navigator !== 'undefined' && /msie [6-9]\\b/.test(navigator.userAgent.toLowerCase());
+const isOldIE = typeof navigator !== 'undefined' &&
+    /msie [6-9]\\b/.test(navigator.userAgent.toLowerCase());
 function createInjector(context) {
-  return function (id, style) {
-    return addStyle(id, style);
-  };
+    return (id, style) => addStyle(id, style);
 }
-var HEAD;
-var styles = {};
-
+let HEAD;
+const styles = {};
 function addStyle(id, css) {
-  var group = isOldIE ? css.media || 'default' : id;
-  var style = styles[group] || (styles[group] = {
-    ids: new Set(),
-    styles: []
-  });
-
-  if (!style.ids.has(id)) {
-    style.ids.add(id);
-    var code = css.source;
-
-    if (css.map) {
-      // https://developer.chrome.com/devtools/docs/javascript-debugging
-      // this makes source maps inside style tags work properly in Chrome
-      code += '\n/*# sourceURL=' + css.map.sources[0] + ' */'; // http://stackoverflow.com/a/26603875
-
-      code += '\n/*# sourceMappingURL=data:application/json;base64,' + btoa(unescape(encodeURIComponent(JSON.stringify(css.map)))) + ' */';
+    const group = isOldIE ? css.media || 'default' : id;
+    const style = styles[group] || (styles[group] = { ids: new Set(), styles: [] });
+    if (!style.ids.has(id)) {
+        style.ids.add(id);
+        let code = css.source;
+        if (css.map) {
+            // https://developer.chrome.com/devtools/docs/javascript-debugging
+            // this makes source maps inside style tags work properly in Chrome
+            code += '\n/*# sourceURL=' + css.map.sources[0] + ' */';
+            // http://stackoverflow.com/a/26603875
+            code +=
+                '\n/*# sourceMappingURL=data:application/json;base64,' +
+                    btoa(unescape(encodeURIComponent(JSON.stringify(css.map)))) +
+                    ' */';
+        }
+        if (!style.element) {
+            style.element = document.createElement('style');
+            style.element.type = 'text/css';
+            if (css.media)
+                style.element.setAttribute('media', css.media);
+            if (HEAD === undefined) {
+                HEAD = document.head || document.getElementsByTagName('head')[0];
+            }
+            HEAD.appendChild(style.element);
+        }
+        if ('styleSheet' in style.element) {
+            style.styles.push(code);
+            style.element.styleSheet.cssText = style.styles
+                .filter(Boolean)
+                .join('\n');
+        }
+        else {
+            const index = style.ids.size - 1;
+            const textNode = document.createTextNode(code);
+            const nodes = style.element.childNodes;
+            if (nodes[index])
+                style.element.removeChild(nodes[index]);
+            if (nodes.length)
+                style.element.insertBefore(textNode, nodes[index]);
+            else
+                style.element.appendChild(textNode);
+        }
     }
-
-    if (!style.element) {
-      style.element = document.createElement('style');
-      style.element.type = 'text/css';
-      if (css.media) style.element.setAttribute('media', css.media);
-
-      if (HEAD === undefined) {
-        HEAD = document.head || document.getElementsByTagName('head')[0];
-      }
-
-      HEAD.appendChild(style.element);
-    }
-
-    if ('styleSheet' in style.element) {
-      style.styles.push(code);
-      style.element.styleSheet.cssText = style.styles.filter(Boolean).join('\n');
-    } else {
-      var index = style.ids.size - 1;
-      var textNode = document.createTextNode(code);
-      var nodes = style.element.childNodes;
-      if (nodes[index]) style.element.removeChild(nodes[index]);
-      if (nodes.length) style.element.insertBefore(textNode, nodes[index]);else style.element.appendChild(textNode);
-    }
-  }
 }
-
-var browser = createInjector;
 
 /* script */
 const __vue_script__$1 = script$1;
@@ -2467,10 +2461,10 @@ const __vue_is_functional_template__$1 = false;
 
 /* style inject shadow dom */
 
-const __vue_component__$1 = normalizeComponent_1({
+const __vue_component__$1 = /*#__PURE__*/normalizeComponent({
   render: __vue_render__,
   staticRenderFns: __vue_staticRenderFns__
-}, __vue_inject_styles__$1, __vue_script__$1, __vue_scope_id__$1, __vue_is_functional_template__$1, __vue_module_identifier__$1, false, browser, undefined, undefined);
+}, __vue_inject_styles__$1, __vue_script__$1, __vue_scope_id__$1, __vue_is_functional_template__$1, __vue_module_identifier__$1, false, createInjector, undefined, undefined);
 
 /** @type VideoImpl */
 var FacebookVideo = {
@@ -2853,10 +2847,10 @@ const __vue_is_functional_template__$2 = false;
 
 /* style inject shadow dom */
 
-const __vue_component__$2 = normalizeComponent_1({
+const __vue_component__$2 = /*#__PURE__*/normalizeComponent({
   render: __vue_render__$1,
   staticRenderFns: __vue_staticRenderFns__$1
-}, __vue_inject_styles__$2, __vue_script__$2, __vue_scope_id__$2, __vue_is_functional_template__$2, __vue_module_identifier__$2, false, browser, undefined, undefined);
+}, __vue_inject_styles__$2, __vue_script__$2, __vue_scope_id__$2, __vue_is_functional_template__$2, __vue_module_identifier__$2, false, createInjector, undefined, undefined);
 
 var EmbettyPlugin = {
   /**
--- dist/embetty-vue.js
+++ dist/embetty-vue.js
@@ -212,7 +212,7 @@
 	(module.exports = function (key, value) {
 	  return sharedStore[key] || (sharedStore[key] = value !== undefined ? value : {});
 	})('versions', []).push({
-	  version: '3.6.4',
+	  version: '3.6.5',
 	  mode:  'global',
 	  copyright: '© 2020 Denis Pushkarev (zloirock.ru)'
 	});
@@ -1976,7 +1976,13 @@
 	    defer = functionBindContext(port.postMessage, port, 1);
 	  // Browsers with postMessage, skip WebWorkers
 	  // IE8 has postMessage, but it's sync & typeof its postMessage is 'object'
-	  } else if (global_1.addEventListener && typeof postMessage == 'function' && !global_1.importScripts && !fails(post)) {
+	  } else if (
+	    global_1.addEventListener &&
+	    typeof postMessage == 'function' &&
+	    !global_1.importScripts &&
+	    !fails(post) &&
+	    location.protocol !== 'file:'
+	  ) {
 	    defer = post;
 	    global_1.addEventListener('message', listener, false);
 	  // IE8-
@@ -2571,90 +2577,80 @@
 	  }
 	};
 
-	function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier
-	/* server only */
-	, shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
-	  if (typeof shadowMode !== 'boolean') {
-	    createInjectorSSR = createInjector;
-	    createInjector = shadowMode;
-	    shadowMode = false;
-	  } // Vue.extend constructor export interop.
-
-
-	  var options = typeof script === 'function' ? script.options : script; // render functions
-
-	  if (template && template.render) {
-	    options.render = template.render;
-	    options.staticRenderFns = template.staticRenderFns;
-	    options._compiled = true; // functional template
-
-	    if (isFunctionalTemplate) {
-	      options.functional = true;
+	function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier /* server only */, shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
+	    if (typeof shadowMode !== 'boolean') {
+	        createInjectorSSR = createInjector;
+	        createInjector = shadowMode;
+	        shadowMode = false;
 	    }
-	  } // scopedId
-
-
-	  if (scopeId) {
-	    options._scopeId = scopeId;
-	  }
-
-	  var hook;
-
-	  if (moduleIdentifier) {
-	    // server build
-	    hook = function hook(context) {
-	      // 2.3 injection
-	      context = context || // cached call
-	      this.$vnode && this.$vnode.ssrContext || // stateful
-	      this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext; // functional
-	      // 2.2 with runInNewContext: true
-
-	      if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
-	        context = __VUE_SSR_CONTEXT__;
-	      } // inject component styles
-
-
-	      if (style) {
-	        style.call(this, createInjectorSSR(context));
-	      } // register component module identifier for async chunk inference
-
-
-	      if (context && context._registeredComponents) {
-	        context._registeredComponents.add(moduleIdentifier);
-	      }
-	    }; // used by ssr in case component is cached and beforeCreate
-	    // never gets called
-
-
-	    options._ssrRegister = hook;
-	  } else if (style) {
-	    hook = shadowMode ? function () {
-	      style.call(this, createInjectorShadow(this.$root.$options.shadowRoot));
-	    } : function (context) {
-	      style.call(this, createInjector(context));
-	    };
-	  }
-
-	  if (hook) {
-	    if (options.functional) {
-	      // register for functional component in vue file
-	      var originalRender = options.render;
-
-	      options.render = function renderWithStyleInjection(h, context) {
-	        hook.call(context);
-	        return originalRender(h, context);
-	      };
-	    } else {
-	      // inject component registration as beforeCreate hook
-	      var existing = options.beforeCreate;
-	      options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+	    // Vue.extend constructor export interop.
+	    const options = typeof script === 'function' ? script.options : script;
+	    // render functions
+	    if (template && template.render) {
+	        options.render = template.render;
+	        options.staticRenderFns = template.staticRenderFns;
+	        options._compiled = true;
+	        // functional template
+	        if (isFunctionalTemplate) {
+	            options.functional = true;
+	        }
 	    }
-	  }
-
-	  return script;
+	    // scopedId
+	    if (scopeId) {
+	        options._scopeId = scopeId;
+	    }
+	    let hook;
+	    if (moduleIdentifier) {
+	        // server build
+	        hook = function (context) {
+	            // 2.3 injection
+	            context =
+	                context || // cached call
+	                    (this.$vnode && this.$vnode.ssrContext) || // stateful
+	                    (this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext); // functional
+	            // 2.2 with runInNewContext: true
+	            if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+	                context = __VUE_SSR_CONTEXT__;
+	            }
+	            // inject component styles
+	            if (style) {
+	                style.call(this, createInjectorSSR(context));
+	            }
+	            // register component module identifier for async chunk inference
+	            if (context && context._registeredComponents) {
+	                context._registeredComponents.add(moduleIdentifier);
+	            }
+	        };
+	        // used by ssr in case component is cached and beforeCreate
+	        // never gets called
+	        options._ssrRegister = hook;
+	    }
+	    else if (style) {
+	        hook = shadowMode
+	            ? function (context) {
+	                style.call(this, createInjectorShadow(context, this.$root.$options.shadowRoot));
+	            }
+	            : function (context) {
+	                style.call(this, createInjector(context));
+	            };
+	    }
+	    if (hook) {
+	        if (options.functional) {
+	            // register for functional component in vue file
+	            const originalRender = options.render;
+	            options.render = function renderWithStyleInjection(h, context) {
+	                hook.call(context);
+	                return originalRender(h, context);
+	            };
+	        }
+	        else {
+	            // inject component registration as beforeCreate hook
+	            const existing = options.beforeCreate;
+	            options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+	        }
+	    }
+	    return script;
 	}
-
-	var normalizeComponent_1 = normalizeComponent;
 
 	/* script */
 	var __vue_script__ = script;
@@ -2678,7 +2674,7 @@
 
 	/* style inject shadow dom */
 
-	var __vue_component__ = normalizeComponent_1({}, __vue_inject_styles__, __vue_script__, __vue_scope_id__, __vue_is_functional_template__, __vue_module_identifier__, false, undefined, undefined, undefined);
+	var __vue_component__ = /*#__PURE__*/normalizeComponent({}, __vue_inject_styles__, __vue_script__, __vue_scope_id__, __vue_is_functional_template__, __vue_module_identifier__, false, undefined, undefined, undefined);
 
 	var LINK_IMAGE_SIZE = 125;
 	var MIN_WINDOW_WIDTH = 600;
@@ -2920,60 +2916,58 @@
 	  }
 	};
 
-	var isOldIE = typeof navigator !== 'undefined' && /msie [6-9]\\b/.test(navigator.userAgent.toLowerCase());
+	const isOldIE = typeof navigator !== 'undefined' &&
+	    /msie [6-9]\\b/.test(navigator.userAgent.toLowerCase());
 	function createInjector(context) {
-	  return function (id, style) {
-	    return addStyle(id, style);
-	  };
+	    return (id, style) => addStyle(id, style);
 	}
-	var HEAD;
-	var styles = {};
-
+	let HEAD;
+	const styles = {};
 	function addStyle(id, css) {
-	  var group = isOldIE ? css.media || 'default' : id;
-	  var style = styles[group] || (styles[group] = {
-	    ids: new Set(),
-	    styles: []
-	  });
-
-	  if (!style.ids.has(id)) {
-	    style.ids.add(id);
-	    var code = css.source;
-
-	    if (css.map) {
-	      // https://developer.chrome.com/devtools/docs/javascript-debugging
-	      // this makes source maps inside style tags work properly in Chrome
-	      code += '\n/*# sourceURL=' + css.map.sources[0] + ' */'; // http://stackoverflow.com/a/26603875
-
-	      code += '\n/*# sourceMappingURL=data:application/json;base64,' + btoa(unescape(encodeURIComponent(JSON.stringify(css.map)))) + ' */';
+	    const group = isOldIE ? css.media || 'default' : id;
+	    const style = styles[group] || (styles[group] = { ids: new Set(), styles: [] });
+	    if (!style.ids.has(id)) {
+	        style.ids.add(id);
+	        let code = css.source;
+	        if (css.map) {
+	            // https://developer.chrome.com/devtools/docs/javascript-debugging
+	            // this makes source maps inside style tags work properly in Chrome
+	            code += '\n/*# sourceURL=' + css.map.sources[0] + ' */';
+	            // http://stackoverflow.com/a/26603875
+	            code +=
+	                '\n/*# sourceMappingURL=data:application/json;base64,' +
+	                    btoa(unescape(encodeURIComponent(JSON.stringify(css.map)))) +
+	                    ' */';
+	        }
+	        if (!style.element) {
+	            style.element = document.createElement('style');
+	            style.element.type = 'text/css';
+	            if (css.media)
+	                style.element.setAttribute('media', css.media);
+	            if (HEAD === undefined) {
+	                HEAD = document.head || document.getElementsByTagName('head')[0];
+	            }
+	            HEAD.appendChild(style.element);
+	        }
+	        if ('styleSheet' in style.element) {
+	            style.styles.push(code);
+	            style.element.styleSheet.cssText = style.styles
+	                .filter(Boolean)
+	                .join('\n');
+	        }
+	        else {
+	            const index = style.ids.size - 1;
+	            const textNode = document.createTextNode(code);
+	            const nodes = style.element.childNodes;
+	            if (nodes[index])
+	                style.element.removeChild(nodes[index]);
+	            if (nodes.length)
+	                style.element.insertBefore(textNode, nodes[index]);
+	            else
+	                style.element.appendChild(textNode);
+	        }
 	    }
-
-	    if (!style.element) {
-	      style.element = document.createElement('style');
-	      style.element.type = 'text/css';
-	      if (css.media) style.element.setAttribute('media', css.media);
-
-	      if (HEAD === undefined) {
-	        HEAD = document.head || document.getElementsByTagName('head')[0];
-	      }
-
-	      HEAD.appendChild(style.element);
-	    }
-
-	    if ('styleSheet' in style.element) {
-	      style.styles.push(code);
-	      style.element.styleSheet.cssText = style.styles.filter(Boolean).join('\n');
-	    } else {
-	      var index = style.ids.size - 1;
-	      var textNode = document.createTextNode(code);
-	      var nodes = style.element.childNodes;
-	      if (nodes[index]) style.element.removeChild(nodes[index]);
-	      if (nodes.length) style.element.insertBefore(textNode, nodes[index]);else style.element.appendChild(textNode);
-	    }
-	  }
 	}
-
-	var browser = createInjector;
 
 	var __vue_script__$1 = script$1;
 	/* template */
@@ -3110,10 +3104,10 @@
 
 	/* style inject shadow dom */
 
-	var __vue_component__$1 = normalizeComponent_1({
+	var __vue_component__$1 = /*#__PURE__*/normalizeComponent({
 	  render: __vue_render__,
 	  staticRenderFns: __vue_staticRenderFns__
-	}, __vue_inject_styles__$1, __vue_script__$1, __vue_scope_id__$1, __vue_is_functional_template__$1, __vue_module_identifier__$1, false, browser, undefined, undefined);
+	}, __vue_inject_styles__$1, __vue_script__$1, __vue_scope_id__$1, __vue_is_functional_template__$1, __vue_module_identifier__$1, false, createInjector, undefined, undefined);
 
 	var aPossiblePrototype = function (it) {
 	  if (!isObject(it) && it !== null) {
@@ -3696,10 +3690,10 @@
 
 	/* style inject shadow dom */
 
-	var __vue_component__$2 = normalizeComponent_1({
+	var __vue_component__$2 = /*#__PURE__*/normalizeComponent({
 	  render: __vue_render__$1,
 	  staticRenderFns: __vue_staticRenderFns__$1
-	}, __vue_inject_styles__$2, __vue_script__$2, __vue_scope_id__$2, __vue_is_functional_template__$2, __vue_module_identifier__$2, false, browser, undefined, undefined);
+	}, __vue_inject_styles__$2, __vue_script__$2, __vue_scope_id__$2, __vue_is_functional_template__$2, __vue_module_identifier__$2, false, createInjector, undefined, undefined);
 
 	var EmbettyPlugin = {
 	  /**
